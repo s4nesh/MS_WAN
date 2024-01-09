9ebba716afa3334102c66dbe4d484db4e2b2e424
Sanesh Gobin <saneshgobin@icloud.com>
2024-01-09 15:08:23 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from icmplib import ping
+import os
+
+host = ping('192.168.0.250', count=4, interval=0.2)
+
+
+typed_quit = False
+
+while not typed_quit:
+    user_input = input("> ").lower()
+    if user_input == 'check gateway':
+        if host.is_alive:
+            print('Can reach gateway')
+        else:
+            print('Host unreachable')
+    elif user_input == 'quit':
+        break
+    elif user_input == "clear":
+        os.system('cls')
+    else:
+        print('Command not found')
+
